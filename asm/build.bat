5878ae327a1b30cc6a882db39d47f850a32b3696
jonathon <jonathon@630ac908-e21a-4104-874e-04c253b3e38a>
2013-05-22 21:00:51 +0000

    Removed the EXIT in build.bat so you can see any error messages.

--- asm/build.bat
+++ asm/build.bat
@@ -2,5 +2,5 @@ SET confpath=%cd%\dosbox_kcpsm3.conf
 
 echo %confpath%
 
-"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -userconf -conf %confpath% -c "KCPSM3.EXE program.psm" -c "EXIT"
+"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -userconf -conf %confpath% -c "KCPSM3.EXE program.psm"
 
