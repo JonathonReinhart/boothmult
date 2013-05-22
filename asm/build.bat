500366849718af2c0726956b40f8c4c270513a5f
jonathon <jonathon@630ac908-e21a-4104-874e-04c253b3e38a>
2013-05-22 16:09:19 +0000

    Updated build.bat to use a DOSBox .conf file. Set CPU options for FAST compilation.

--- asm/build.bat
+++ asm/build.bat
@@ -1,1 +1,6 @@
-"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -c "MOUNT C ." -c "C:" -c "KCPSM3.EXE program.psm" -c "EXIT"
+SET confpath=%cd%\dosbox_kcpsm3.conf
+
+echo %confpath%
+
+"C:\Program Files (x86)\DOSBox-0.74\DOSBox.exe" -userconf -conf %confpath% -c "KCPSM3.EXE program.psm" -c "EXIT"
+
